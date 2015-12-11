d1d5bd87e50b646bd59900317ccb3305a3d66a83
MinHo Kim <min.ho.kim@samsung.com>
2015-12-11 21:08:59 +0900

    intruducing Eclipse UI Test by SWTBot  - add test for dexter-eclipse-ui plug-in

---
+++ project/dexter-eclipse-ui/src/test/com/samsung/sec/dexter/eclipse/ui/UIActivatorTest.java
@@ -0,0 +1,35 @@
+package com.samsung.sec.dexter.eclipse.ui;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class UIActivatorTest {
+	private static SWTWorkbenchBot bot = new SWTWorkbenchBot();
+	
+	@Before
+	public void setup(){
+		bot.sleep(3000);
+		bot.button("OK").click();
+		bot.button("Cancel").click();
+		bot.button("OK").click();
+		bot.viewByTitle("Welcome").show();
+		bot.viewByTitle("Welcome").close();
+		bot.viewByTitle("Project Explorer").show();
+	}
+	
+	@Test
+	public void login_failure(){
+		bot.toolbarButtonWithTooltip("Login to Dexter Server (Coding-time Static Analysis Server) (Ctrl+Alt+D)").click();
+		bot.textWithLabel("Single ID:").setText("wrong-id");
+		bot.textWithLabel("Password:").setText("wrongpassowrd");
+		bot.textWithLabel("Dexter Server(IP:Port):").setText("wrongip:123");
+		bot.button("접속 테스트").click();
+		bot.button("OK").click();
+		bot.button("Cancel").click();
+	}
+
+}
