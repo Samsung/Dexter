99539679e735a93de0726ea653948bac4c358e9d
Minjung Baek <minjung.baek@samsung.com>
2016-09-10 23:19:15 +0900

    Bug Fix: userService on dexter-monitor

--- project/dexter-monitor/public/service/userService.js
+++ project/dexter-monitor/public/service/userService.js
@@ -49,6 +49,7 @@ monitorApp.service('UserService', function($http, $log, $q) {
     this.getUserList = function() {
         return $http.get('/api/v2/user')
             .then((res) => {
+                console.log(res);
                 if (!isHttpResultOK(res)) {
                     $log.error('Failed to get user list');
                     return [];
