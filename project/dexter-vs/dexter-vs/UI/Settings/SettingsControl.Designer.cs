5a251ad8963374ebce237d4c306d44466a54b8de
Karol Antczak <k.antczak@samsung.com>
2017-03-30 15:17:34 +0200

    Added scrollbar to VS plugin Options page.

--- project/dexter-vs/dexter-vs/UI/Settings/SettingsControl.Designer.cs
+++ project/dexter-vs/dexter-vs/UI/Settings/SettingsControl.Designer.cs
@@ -299,6 +299,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.Controls.Add(this.dexterSettingsGroupBox);
             this.Controls.Add(this.serverSettingsGroupBox);
             this.Name = "SettingsControl";
