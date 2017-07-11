5571fc0c7fd980b74371c6bf95b70531083bb909
JanSawicki <j.sawicki@samsung.com>
2017-07-11 10:30:49 +0200

    MetricUtilTest update
    Added more tests for counting the actual (without comments) number of lines of code.

--- project/dexter-metrics/src/test/com/samsung/sec/dexter/metrics/util/MetricUtilTest.java
+++ project/dexter-metrics/src/test/com/samsung/sec/dexter/metrics/util/MetricUtilTest.java
@@ -1,13 +1,20 @@
 package com.samsung.sec.dexter.metrics.util;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+
 //import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.samsung.sec.dexter.core.exception.DexterRuntimeException;
 import com.samsung.sec.dexter.metrics.util.MetricUtil;
 
 public class MetricUtilTest {
-
+	String TestFilePath1 = ".\\src\\test\\com\\samsung\\sec\\dexter\\metrics\\util\\Test_file_for_MetricUtilTest1.txt";
+	String TestFilePath2 = ".\\src\\test\\com\\samsung\\sec\\dexter\\metrics\\util\\Test_file_for_MetricUtilTest2.txt";
+	
 	@Test
 	public void test() {
 		final String source = "void main()\n" 
@@ -28,5 +35,80 @@ public class MetricUtilTest {
 		
 	//MetricUtil.getFunctionLOCArray(source, 0, 14);
 	}
-
+	
+	@Test
+	public void MetricUtilTest_NonexistantFile() {
+		MetricUtil tester = new MetricUtil();
+		int start = 0;
+		int end = 10;
+		DexterRuntimeException exception = null;
+		try {
+			tester.getFunctionLOCArray(".\\ThereIsNoSuchFile.nope", start, end);
+		}
+		catch(DexterRuntimeException e) {
+			exception=e;
+			if(exception==null) {
+				Assert.fail();
+			}
+		}
+	}
+	
+	@Test
+	public void MetricUtilTest_EmptyFilePath() {
+		MetricUtil tester = new MetricUtil();
+		int start = 0;
+		int end = 10;
+		tester.getFunctionLOCArray("", start, end);
+	}
+	
+	@Test
+	public void MetricUtilTest_StartGreaterThanEnd() {
+		MetricUtil tester = new MetricUtil();
+		int start = 4;
+		int end = 2;
+		tester.getFunctionLOCArray(TestFilePath1, start, end);
+	}
+	
+	@Test
+	public void MetricUtilTest_StartNegative() {
+		MetricUtil tester = new MetricUtil();
+		int start = -10;
+		int end = 2;
+		tester.getFunctionLOCArray(TestFilePath1, start, end);
+	}
+	
+	@Test
+	public void MetricUtilTest_TooLargeEnd() {
+		MetricUtil tester = new MetricUtil();
+		int start = 0;
+		int end = 10000;
+		int loc=0;
+		loc=tester.getFunctionLOCArray(TestFilePath1, start, end);
+		
+		assertEquals(loc, 7);
+	}
+	
+	@Test
+	public void MetricUtilTest_ProperExample1() {
+		MetricUtil tester = new MetricUtil();
+		int start = 2;
+		int end = 14;
+		int loc=0;
+		loc=tester.getFunctionLOCArray(TestFilePath1, start, end);
+		
+		assertEquals(loc, 6);
+	}
+	
+	@Test
+	public void MetricUtilTest_ProperExample2() {
+		MetricUtil tester = new MetricUtil();
+		int start = 0;
+		int end = 21;
+		int loc=0;
+		loc=tester.getFunctionLOCArray(TestFilePath2, start, end);
+		
+		assertEquals(loc, 12);
+	}
+	
+	
 }
