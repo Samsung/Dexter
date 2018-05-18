ef7f706f1a5d2db7cf36555a6ffee056db324e1d
Veronika Sanko <v.sanko@samsung.com>
2018-05-18 16:14:53 +0200

    started to add dexter.cng cofigs

--- dexter-jenkins/dexter-jenkins/src/main/java/io/jenkins/plugins/dexter/DexterPublisher.java
+++ dexter-jenkins/dexter-jenkins/src/main/java/io/jenkins/plugins/dexter/DexterPublisher.java
@@ -16,19 +16,37 @@ import org.kohsuke.stapler.QueryParameter;
 
 import javax.servlet.ServletException;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
 
 
 public class DexterPublisher extends Recorder {
 
     private final String path;
+    private final String pathConfig;
 
-    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    public String getPathConfig() {
+		return pathConfig;
+	}
+
+	// Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public DexterPublisher(String path) {
+    public DexterPublisher(String path, String pathConfig) {
         this.path = path;
+        this.pathConfig = pathConfig;
     }
 
     /**
@@ -46,7 +64,11 @@ public class DexterPublisher extends Recorder {
           message="Hello, " + path + "!";
           Runtime runtime = Runtime.getRuntime();
           try {
-
+        	  boolean fileRead = readFile();
+              if (fileRead) {
+                  replacement();
+                  writeToFile();
+              }
               String command = "cmd /c start cmd.exe /K "  + "\"cd " + path + " && D: && dexter.bat user dexter\"";
               message = command;
                           
@@ -63,10 +85,61 @@ public class DexterPublisher extends Recorder {
 
         return true;
     }
-
-    // Overridden for better type safety.
-    // If your plugin doesn't really define any property on Descriptor,
-    // you don't have to do this.
+    
+    static StringBuffer stringBufferOfData = new StringBuffer();
+    static String filename = null;
+    static Scanner sc = new Scanner(System.in, "UTF-8");
+   
+    private boolean readFile() {
+    	
+       filename = pathConfig;        
+        Scanner fileToRead = null;
+        try {
+            fileToRead = new Scanner(new File(filename), "UTF-8"); 
+            for (String line; fileToRead.hasNextLine() && (line = fileToRead.nextLine()) != null; ) {
+                System.out.println(line);
+                stringBufferOfData.append(line).append("\r\n");
+            }
+            fileToRead.close();
+            return true;
+        } catch (FileNotFoundException ex) {
+            System.out.println("The file " + filename + " could not be found! " + ex.getMessage());
+            return false;
+        } finally {
+        	if (fileToRead != null) {
+            fileToRead.close();
+        	}
+            return true;
+        }
+       
+    }
+    private  void writeToFile() {
+    	OpenOption myOpt = StandardOpenOption.CREATE;
+        try {
+        	Path fileToWrite = Paths.get(pathConfig);
+            BufferedWriter bufwriter = Files.newBufferedWriter(fileToWrite, Charset.forName("UTF-8"),  myOpt);
+            bufwriter.write("\n{");
+            bufwriter.write("\"dexterHome\\\":\\\"\"D:\\Dexter\\Dexter_client\\\", \n"); 
+            bufwriter.write("\"dexterHome\\\":\\\"\"D:\\Dexter\\Dexter_client\\\", \n"); 
+            bufwriter.write("\"dexterHome\\\":\\\"\"D:\\Dexter\\Dexter_client\\\", \n"); 
+            bufwriter.close();
+        } catch (Exception e) {
+            System.out.println("Error occured while attempting to write to file: " + e.getMessage());
+        }
+    }
+    private  void replacement() {
+        readFile();
+       // String lineToEdit = "{";    
+        //int replacementText1 = stringBufferOfData.length();
+       // String replacementText = Integer.toString(replacementText1);
+        // " 
+      
+     //   int startIndex = stringBufferOfData.indexOf(lineToEdit);
+    //    int endIndex = stringBufferOfData.toString().length();
+       // stringBufferOfData.
+       
+    }
+    
     @Override
     public DescriptorImpl getDescriptor() {
         return (DescriptorImpl) super.getDescriptor();
