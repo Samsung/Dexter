2a43996d96c0ab666be3748e112faa78d4ddf153
Veronika Sanko <v.sanko@samsung.com>
2018-07-18 09:44:36 +0200

    Add Space rule classes

---
+++ project/dexter-cs/DexterCRC/Src/Crc/SpaceCRC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DexterCRC.Src.CheckerLogic;
+using DexterCS;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DexterCRC.Src.Crc
+{
+    class SpaceCRC : ICRCLogic
+    {
+        SpaceRules spaceRules;
+
+        public SpaceCRC()
+        {
+            spaceRules = new SpaceRules();
+        }
+
+        public void Analyze(AnalysisConfig config, AnalysisResult result, Checker checker, SyntaxNode syntaxRoot)
+        {
+            var methodRaws = syntaxRoot.DescendantNodes().OfType<MethodDeclarationSyntax>();
+            if (!methodRaws.Any())
+            {
+                return;
+            }
+
+            foreach (var methodRaw in methodRaws)
+            {
+                
+                if (methodRaw.GetLeadingTrivia().ToList().Count == 0)
+                {
+                    spaceRules.HasDefect(true);
+                    PreOccurence preOcc = spaceRules.MakeDefect(config, checker, methodRaw);
+                    result.AddDefectWithPreOccurence(preOcc);
+                }
+            }
+        }
+    }
+}
