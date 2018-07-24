c753bb012efb5d7d8163925a6f69ff0a7e9acacc
JanSawicki <jansawickisawicki@gmail.com>
2018-07-24 14:01:43 +0200

    Changed the DTE2 object acquisition to be more flexible and independent of Visual Studio version

--- project/dexter-vs/Dexter/Common/Core/Utils/LanguageDetector.cs
+++ project/dexter-vs/Dexter/Common/Core/Utils/LanguageDetector.cs
@@ -1,5 +1,6 @@
 ﻿using EnvDTE;
 using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
 using System;
 
 namespace Dexter.Common.Utils
@@ -8,10 +9,10 @@ namespace Dexter.Common.Utils
     {
         public static bool IsCodeModelLanguageCSharp(string projectFullPath)
         {
-            DTE2 dte2 = GetDTE2Object();
+            DTE2 dte2 = (DTE2)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
 
-            if (dte2.DTE.ActiveDocument.ProjectItem.ContainingProject
-                .CodeModel.Language == CodeModelLanguageConstants.vsCMLanguageCSharp)
+            if (dte2.DTE.ActiveDocument.ProjectItem.ContainingProject.CodeModel.Language 
+                == CodeModelLanguageConstants.vsCMLanguageCSharp)
             {
                 return true;
             }
