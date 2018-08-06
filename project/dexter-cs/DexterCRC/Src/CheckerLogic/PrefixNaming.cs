f37a0a48199ee48f35d721f943e9d4c451db8f86
JanSawicki <jansawickisawicki@gmail.com>
2018-08-06 12:44:32 +0200

    Fixed the logic of HasPrefix() and HasDefect() methods

--- project/dexter-cs/DexterCRC/Src/CheckerLogic/PrefixNaming.cs
+++ project/dexter-cs/DexterCRC/Src/CheckerLogic/PrefixNaming.cs
@@ -16,7 +16,7 @@ namespace DexterCRC
         public bool HasDefect(object value)
         {
             var namingSet = (NamingSet)value;
-            return DexterCRCUtil.HasPrefix(namingSet.currentName, namingSet.basicWord);
+            return !DexterCRCUtil.HasPrefix(namingSet.currentName, namingSet.basicWord);
         }
 
         public PreOccurence MakeDefect(AnalysisConfig config, Checker checker, CSharpSyntaxNode raw)
