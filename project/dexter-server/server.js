47c25b848d5712ac910a195aa7bb18fb9646fa0d
Veronika Sanko <v.sanko@samsung.com>
2018-10-29 09:23:42 +0100

    Add swagger dist folder and swagger configuration files

--- project/dexter-server/server.js
+++ project/dexter-server/server.js
@@ -23,7 +23,6 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 var express = require('express');
 var path = require('path');
 var http = require('http');
@@ -43,6 +42,51 @@ var adminSE = require("./routes/adminSE");
 
 var app = express();
 
+var argv = require('minimist')(process.argv.slice(2));
+var bodyParser = require('body-parser');
+var subpath = express();
+	app.use(bodyParser());
+	app.use("/v1", subpath);
+
+	var swagger = require('swagger-node-express').createNew(subpath);
+app.use(express.static('dist'));
+swagger.setApiInfo({
+	    title: "example API",
+	    description: "API to do something, manage something...",
+	    termsOfServiceUrl: "",
+	    contact: "yourname@something.com",
+	    license: "",
+	    licenseUrl: ""
+	});
+app.get('/', function (req, res) {
+	    res.sendFile(__dirname + '/dist/index.html');
+	});
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+	// Configure the API domain
+	var domain = 'localhost';
+	if(argv.domain !== undefined)
+	    domain = argv.domain;
+	else
+	    console.log('No --domain=xxx specified, taking default hostname "localhost".')
+
+	// Configure the API port
+	var port = 3000;
+	if(argv.port !== undefined)
+	    port = argv.port;
+	else
+	    console.log('No --port=xxx specified, taking default port ' + port + '.')
+
+	// Set and display the application URL
+	var applicationUrl = 'http://' + domain + ':' + port+ "";
+	console.log('snapJob API running on ' + applicationUrl);
+
+
+	swagger.configure(applicationUrl, '1.0.0');
+
+
+	// Start the web server
+	app.listen(port);
 global.runOptions = {
     port:4982,
     databaseHost:'localhost',
